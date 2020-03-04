9332a0fa2b352258e12b1d101f98003bb8d7efbd
Kevin Wang <1875689740ws@gmail.com>
2020-03-03 21:08:37 -0800

    Update activate.js

--- miniprogram/pages/activate/activate.js
+++ miniprogram/pages/activate/activate.js
@@ -3,6 +3,7 @@
 
 
 Page({
+
   data: {
     cardNum: '',
   },
@@ -13,11 +14,66 @@ Page({
   },
 
   registerOnClick: function (e) {
-    console.log("card number: " + this.data.cardNum),
-    console.log(e)
-    wx.setStorage({
-      key: 'card number',
-      data: this.data.cardNum
+    var that = this
+    const db = wx.cloud.database()
+    db.collection("card").where({
+      card_number: that.data.cardNum
+    })
+    .get({
+      success: function(res){
+        if (res.data.length == 0){
+          wx.showToast({
+            title: 'Invalid',
+            icon: 'none',
+            duration: 2000
+          })
+          return
+        }
+      }
+    }),
+    db.collection("used").where({
+      card_number: that.data.cardNum
+    })
+    .get({
+      success: function(res){
+        console.log(res.data)
+        if (res.data.length == 1){
+          wx.showToast({
+            title: 'Used',
+            icon: 'none',
+            duration: 2000
+          })
+        } else {
+          wx.showToast({
+            title: 'Activated',
+            duration: 2000
+          })
+          wx.setStorage({
+            key: 'card number',
+            data: that.data.cardNum
+          }),
+          db.collection("activation").add({
+            data: {
+              card_number: that.data.cardNum,
+              activation_date: new Date()
+            }
+          })
+          .then(res => {
+            console.log(res)
+          }),
+          db.collection("used").add({
+            data: {
+              card_num: that.data.cardNum
+            }
+          })
+          .then(res => {
+            console.log(res)
+          })
+        }
+      },
+      fail: function(res){
+        console.log(res.data)
+      }
     })
   }
 })
